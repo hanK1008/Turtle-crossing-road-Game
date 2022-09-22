d3de87c4f91c0f572a8b4e08cb5036025ee7b2db
hanK_laptop <hanK_laptop@python.com>
2022-09-23 00:09:36 +0530

    Worked on scoreboard and game over sequence. completed the project.

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ while game_is_on:
     for car in car_manager.all_cars:
         if player.distance(car) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
 # Leveling Up
     if player.ycor() >= 270:
--- scoreboard.py
+++ scoreboard.py
@@ -1,25 +1,23 @@
-from turtle import Turtle
-FONT = ("Courier", 24, "normal")
 
+FONT = ("Courier", 24, "normal")
+from turtle import Turtle
 
 class Scoreboard(Turtle):
-
-    def __int__(self):
+    def __init__(self):
         super().__init__()
         self.score = 1
         self.color("black")
+        self.penup()
+        self.hideturtle()
         # self.update_level()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
+
 
     def update_level(self):
         self.clear()
-        self.penup()
-        self.hideturtle()
         self.goto(-220, 250)
-        # self.score += 1
         self.write(f"Level: {self.score}", align="center", font=FONT)
         self.score += 1
-        # self.level_no += 1
-        # self.level += 1
-
-
-        #Work on attribute game_level its not working
